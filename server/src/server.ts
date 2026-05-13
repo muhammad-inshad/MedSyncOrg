f4c1f8cfe0242e1d3a084c808d060a3d654401b2
Inshad <minshad010@gmail.com>
2026-05-13 18:13:34 +0530

    fix port

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,13 +9,13 @@ import http from "http";
 import logger from "./utils/logger.ts";
 
 connectDB();
+const PORT = Number(process.env.PORT) || 5000;
 
-const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
 initSocket(server);
 
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
   logger.info(`Server running on http://localhost:${PORT}`);
 });
