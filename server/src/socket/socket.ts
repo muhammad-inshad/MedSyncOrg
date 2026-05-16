17467103a94f242e25fdc0e5b08b135cb9f0887f
Inshad <minshad010@gmail.com>
2026-05-16 11:02:25 +0530

    fix live token issue

--- server/src/socket/socket.ts
+++ server/src/socket/socket.ts
@@ -5,7 +5,7 @@ export const initSocket = (server: HTTPServer) => {
   console.log("Initializing Socket.io...");
   const io = new Server(server, {
     cors: {
-      origin: true,
+      origin: [process.env.FRONTEND_URL || 'http://localhost:5173', "https://accounts.google.com"],
       methods: ["GET", "POST"],
       credentials: true
     },
